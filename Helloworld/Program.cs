6795237c82036bf8929bf2d50951ad6693d671d8
catimental <suny970311@gmail.com>
2021-03-08 09:41:29 +0900

    Add files via upload

---
+++ Helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp World!!");
+            Console.WriteLine("학번 이름");
+            
+        }
+    }
+}
