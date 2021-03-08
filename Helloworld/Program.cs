99ce9d25eeb3da7707f5263a99912c711480b476
catimental <suny970311@gmail.com>
2021-03-08 09:42:23 +0900

    Update Program.cs

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -4,12 +4,10 @@ namespace HelloWorld
 {
     class Program
     {
-        
+        https://github.com/catimental/CSharp/blob/main/Helloworld/Program.cs
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello CSharp World!!");
+        {            
             Console.WriteLine("학번 이름");
-            
         }
     }
 }
