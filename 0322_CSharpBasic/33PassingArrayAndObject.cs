93bfe34dcf14e175120a00d26cf25437a239bfec
catimental <suny970311@gmail.com>
2021-03-22 10:56:39 +0900

    0322 강의

---
+++ 0322_CSharpBasic/33PassingArrayAndObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Basic
+{
+    class ValueAndReference
+    {
+        static void Main(string[] args)
+        {
+            string s = "before passing";
+            Console.WriteLine(s);
+            Test(s);
+            Console.WriteLine(s);
+            Test(ref s);
+            Console.WriteLine(s);
+        }
+        public static void Test(string s)
+        {
+            s = "after passing";
+        }
+        public static void Test(ref string s)
+        {
+            s = "after passing";
+        }
+
+    }
+}
