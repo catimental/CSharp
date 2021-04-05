01bb3169137f9d6734aa70fbe08b20c7d93b9eb3
catimental <suny970311@gmail.com>
2021-04-05 20:57:09 +0900

    0405 pdf7 실습

---
+++ 0405_pdf7/10_OverloadedConstructorApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OverloadedConstructorApp
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Fraction(int n)
+        {
+            numerator = n;
+            denominator = 1;
+        }
+        public Fraction(int n, int d)
+        {
+            numerator = n;
+            denominator = d;
+        }
+
+        public override string ToString()
+        {
+            return (numerator + "/" + denominator);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(), f2 = new Fraction(2),
+                f3 = new Fraction(1, 2);
+            Console.WriteLine("f1 = {0}, f2 = {1}, f3 = {2}", f1, f2, f3);
+        }
+    }
+}
---
+++ 0405_pdf7/15_RWOnlyPropertyApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RWOnlyPropertyApp
+{
+
+    class RWOnlyProperty
+    {
+        private int readOnlyField = 100;
+        private int writeOnlyField;
+        public int ReadOnlyProperty
+        {
+            get
+            {
+                Console.WriteLine("In the ReadOnlyProperty ...");
+                return readOnlyField;
+            }
+            set
+            {
+                Console.WriteLine("In the WriteOnlyProperty ...");
+                writeOnlyField = value;
+            }
+        }
+        public int WriteOnlyProperty
+        {
+            set
+            {
+                Console.WriteLine("In the WriteOnlyProperty ...");
+                writeOnlyField = value;
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RWOnlyProperty obj = new RWOnlyProperty();
+            obj.WriteOnlyProperty = obj.ReadOnlyProperty;
+            //Console.WriteLine("value = " + obj.writeOnlyField);
+        }
+    }
+}
